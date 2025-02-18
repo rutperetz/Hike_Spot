d99a03e7bd042612c72b513cc0897c2d64bba3a7
Lilach Shekter <Lilach.Shekter@gmail.com>
2025-02-18 19:52:28 +0200

    declaring dependencies in gradle for nav_graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
 
 plugins {
+    // Kotlin serialization plugin for type safe routes and navigation arguments
+    kotlin("plugin.serialization") version "2.0.21"
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("androidx.navigation.safeargs") apply true
 }
 
 android {
@@ -43,11 +46,18 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.circleimageview)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.cardview)
+
+    //navigation implementation:
+    implementation("androidx.navigation:navigation-fragment:2.8.7")
+    implementation("androidx.navigation:navigation-ui:2.8.7")
+
 
 }
